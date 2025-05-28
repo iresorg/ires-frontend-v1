8dd6012b36e3c76fe6b96c68b59b37e6a01c28b9
mjmandelah07 <aramidemojisolagrace@gmail.com>
2025-05-28 19:46:17 +0100

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -106,7 +106,7 @@ export default function HomePage() {
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <Link
               href="/responders"
-              className="rounded-md bg-primary-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
+              className="rounded-md bg-primary-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary-500 focus-visible:outline  focus-visible:outline-offset-2 focus-visible:outline-primary-600"
             >
               Become a Responder
             </Link>
